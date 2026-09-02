eb67d74fbaad39c822424cf6b45173821ee041a0
ManarALHamad <manaralhamad2004@gmail.com>
2026-09-02 09:58:59 +0300

    final commit

--- api/urls.py
+++ api/urls.py
@@ -16,7 +16,6 @@ urlpatterns = [
     path("products/<int:product_id>/variants", views.product_variant_create),
     path("products/<int:product_id>/variants/<int:variant_id>", views.product_variant_detail),
     path("orders", views.order_list_create, name="order-list-create"),
-    path("orders/<int:order_id>", views.order_detail, name="order-detail"),
     path("orders/mine", views.order_list_mine, name="order-list-mine"),
     path("orders/all", views.order_list_all, name="order-list-all"),
 
